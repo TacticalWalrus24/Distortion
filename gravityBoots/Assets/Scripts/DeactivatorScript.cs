350d2c25b74ce6111b991d6fa6cae3705fea435c
TacticalWalrus <vaughanbunt@gmail.com>
2019-10-01 19:41:30 +1000

    arsdgg

---
+++ gravityBoots/Assets/Scripts/DeactivatorScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivatorScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] objects;
+    [SerializeField]
+    Transform button;
+
+    bool activated = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.GetComponent<WeightedButtonScript>().triggered && !activated)
+        {
+            StartCoroutine("DeActivate");
+        }
+        else if (!button.GetComponent<WeightedButtonScript>().triggered && activated)
+        {
+            StartCoroutine("Activate");
+        }
+    }
+
+    IEnumerator Activate()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (!objects[i].gameObject.activeSelf)
+                {
+                    objects[i].gameObject.SetActive(true);
+                }
+
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        activated = true;
+    }
+
+    IEnumerator DeActivate()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].gameObject.activeSelf)
+                {
+                    objects[i].gameObject.SetActive(false);
+                }
+
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        activated = false;
+    }
+}
---
+++ gravityBoots/Assets/Scripts/TriggerDelete.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDelete : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine("DeActivate");
+        }
+    }
+
+    IEnumerator DeActivate()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].gameObject.activeSelf)
+                {
+                    objects[i].gameObject.SetActive(false);
+                }
+
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        gameObject.SetActive(false);
+    }
+}
